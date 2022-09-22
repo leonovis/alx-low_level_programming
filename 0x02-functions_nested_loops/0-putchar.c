b32ed2a88f2d388519ad7dd9c3ca795c492d921d
Leonard Akpofure <la.shivure@gmail.com>
2022-09-22 15:23:21 -0700

    pl

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /*
- * main - Prints _putchar as a message.
- *
- * Return: Always 0 (Success)
- */
+* main - Prints _putchar as a message.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 		int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
